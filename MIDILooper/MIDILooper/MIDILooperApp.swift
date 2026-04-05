d9d635a0fb71434b6bcda3244fc4a323600a9411
Maksim Popov <drowpmv@gmail.com>
2026-04-05 13:16:22 +0200

    feat: add CoreMIDI thru POC

--- MIDILooper/MIDILooper/MIDILooperApp.swift
+++ MIDILooper/MIDILooper/MIDILooperApp.swift
@@ -12,6 +12,7 @@ struct MIDILooperApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
--- MIDILooper/MIDILooperTests/MIDILooperTests.swift
+++ MIDILooper/MIDILooperTests/MIDILooperTests.swift
@@ -10,29 +10,21 @@ import XCTest
 
 final class MIDILooperTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testParserKeepsNoteOnAndNoteOff() {
+        let messages = MIDIMessageParser.forwardedMessages(from: [0x90, 60, 100, 0x80, 60, 0])
+
+        XCTAssertEqual(messages.map(\.rawBytes), [[0x90, 60, 100], [0x80, 60, 0]])
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testParserKeepsSustainButDropsOtherControlChanges() {
+        let messages = MIDIMessageParser.forwardedMessages(from: [0xB0, 1, 64, 0xB0, 64, 127])
+
+        XCTAssertEqual(messages.map(\.rawBytes), [[0xB0, 64, 127]])
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-    }
+    func testParserUsesRunningStatus() {
+        let messages = MIDIMessageParser.forwardedMessages(from: [0x90, 60, 110, 64, 120])
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(messages.map(\.rawBytes), [[0x90, 60, 110], [0x90, 64, 120]])
     }
-
 }
