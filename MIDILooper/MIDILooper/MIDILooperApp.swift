d49b737d94728f99ae201d1a40a7d76924c79afb
Maksim Popov <drowpmv@gmail.com>
2026-04-05 12:33:52 +0200

    chore: add initial iOS app project

---
+++ MIDILooper/MIDILooper/MIDILooperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDILooperApp.swift
+//  MIDILooper
+//
+//  Created by maksim on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MIDILooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
